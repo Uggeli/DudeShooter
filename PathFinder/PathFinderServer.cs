02b0264ebc12d0b2b2aab810eb1db527b4320376
uggeli <sakkisen.ville@gmail.com>
2022-04-05 12:22:46 +0300

    initial

---
+++ PathFinder/PathFinderServer.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public class PathFinderServer : Node
+{
+    HashSet<WorldChunk> Chunks = new HashSet<WorldChunk>();
+    
+}
+
+public class PathFinderChunk
+{
+
+}
+
